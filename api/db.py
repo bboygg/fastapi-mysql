ed45aa96a5263796845f43491482b93bff1acecf
bboygg <bboygg94@gmail.com>
2024-06-06 20:35:39 +0900

    fix db connection change create_engine to create_async_engine

--- api/db.py
+++ api/db.py
@@ -1,11 +1,10 @@
-from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base 
 
 
 ASYNC_DB_URL = "mysql+aiomysql://root@db:3306/demo?charset=utf8"
 
-async_engine = create_engine(ASYNC_DB_URL, echo=True)
+async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
 async_session = sessionmaker(autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession)
 
 
